a5c7164788d1944d103b153aa0e4be9f08c24a11
PaulSigua <mateosigua2002@gmail.com>
2024-06-09 22:43:19 -0500

    cambios

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -1,5 +1,5 @@
 // Creacion de una consonante para conectarnos con el servidor widfly
 
 export const environment = {
-    WA_PATH: 'http://localhost:8080/demo64/rs',
+    WA_PATH: 'http://34.45.101.112:8080/demo64/rs',
 }
